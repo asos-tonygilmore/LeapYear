237b15632aa8382615e943959d9366755e49bc0b
Tony Gilmore <tony.gilmore@asos.com>
2024-01-05 13:56:36 +0000

    first failing test

--- LeapYear.UnitTests/LeapYearCalculatorShould.cs
+++ LeapYear.UnitTests/LeapYearCalculatorShould.cs
@@ -1,8 +1,19 @@
+using LeapYear;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class LeapYearCalculatorShould
     {
+
+        [Test]
+        public void GivenIsYearDivisibleBy4_ThenReturnTrue()
+        {
+            var calculator = new LeapYearCalculator();
+            var result = calculator.IsLeapYear(2000);
+            Assert.IsTrue(result);
+        }
+
+      
     }
 }
---
+++ LeapYear/LeapYearCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace LeapYear
+{
+    public class LeapYearCalculator
+    {
+        public bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
